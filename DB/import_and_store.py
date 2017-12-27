1b2c478b6f8f9b0abe18db364bd53e1a63dd9fd3
elvijs <elvijsdr@gmail.com>
2017-12-27 22:05:19 +0000

    Corrected the games import script

--- DB/import_and_store.py
+++ DB/import_and_store.py
@@ -1,6 +1,6 @@
 import os
-from pgnparser.pgn import GameIterator
-import settings.settings as csettings
+import settings
+from lib.pgnparser.pgn import GameIterator
 import storage.games as store
 
 __author__ = 'elvijs'
@@ -8,7 +8,7 @@ __author__ = 'elvijs'
 if __name__ == "__main__":
     mongo = store.Mongo()
     count = 0
-    for game in GameIterator(os.path.join(csettings.BASEDIR, 'DB/OTB-HQ.pgn')):
+    for game in GameIterator(os.path.join(settings.BASEDIR, 'DB/OTB-HQ.pgn')):
         mongo.store_game(game.to_dict())
         count += 1
         if count % 1000 == 0:
