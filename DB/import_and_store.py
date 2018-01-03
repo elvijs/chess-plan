5196af8b79f47ca4e7e67aca55332ed808dc7bfc
elvijs <elvijsdr@gmail.com>
2018-01-03 10:52:54 +0000

    More explicit encoding handling

--- DB/import_and_store.py
+++ DB/import_and_store.py
@@ -10,12 +10,13 @@ from storage.games import Mongo
 __author__ = 'elvijs'
 
 PGN_PATH = 'DB/all_games.pgn'
+ENCODING = 'iso-8859-1'  # get via `file -i $PGN_PATH`
 
 
 if __name__ == "__main__":
     mongo_client = Mongo()
     count = 0
-    for game in GameIterator(os.path.join(settings.BASEDIR, PGN_PATH)):
+    for game in GameIterator(os.path.join(settings.BASEDIR, PGN_PATH), encoding='iso-8859-1'):
         mongo_client.store_game(game.to_dict())
         count += 1
         if count % 1000 == 0:
